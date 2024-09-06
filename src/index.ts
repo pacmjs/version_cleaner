b6bea6fe6be2b185fc5e00eb88fbef34c9f09ecc
Jonas Franke <jonasfranke@sdevs.org>
2024-09-06 19:56:59 +0200

    chore: Update package.json and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,74 @@
-import { charactersToReplaceInVersion } from "";
-import { getLatestVersion, getLatestVersionForRange } from "./getLatestVersion.js";
+import { charactersToReplaceInVersion } from "pkg/constants";
+import { getLatestVersion, getLatestVersionForRange } from "pkg/latest_version";
 import chalk from "chalk";
 import semver from "semver";
 
-export const cleanVersion = async (version: string, peerDependencies?: { [key: string]: string }) => {
-  if (charactersToReplaceInVersion.includes(version[0])) {
-    version = version.slice(1);
-  }
-
-  if (version === "*") {
-    return "latest";
-  }
-
-  if (version.endsWith(".x")) {
-    const majorVersion = version.split(".")[0];
-    try {
-      const latestVersion = await getLatestVersion(majorVersion);
-      return latestVersion;
-    } catch (error: any) {
-      console.error(chalk.red(`Failed to get the latest version for ${version}:`), error.message);
-      return version.replace(".x", "");
+export const cleanVersion = async (
+    version: string,
+    peerDependencies?: { [key: string]: string },
+) => {
+    if (charactersToReplaceInVersion.includes(version[0])) {
+        version = version.slice(1);
     }
-  }
 
-  if (/^[><=^~]/.test(version)) {
-    try {
-      const [packageName, range] = version.split(/(?<=^\S+)\s/);
-      const latestVersion = await getLatestVersionForRange(packageName, range);
-      return latestVersion;
-    } catch (error: any) {
-      console.error(chalk.red(`Failed to get the latest version for range ${version}:`), error.message);
-      return "latest";
+    if (version === "*") {
+        return "latest";
     }
-  }
 
-  if (peerDependencies) {
-    const peerDepVersion = peerDependencies[version];
-    if (peerDepVersion) {
-      return peerDepVersion;
+    if (version.endsWith(".x")) {
+        const majorVersion = version.split(".")[0];
+        try {
+            const latestVersion = await getLatestVersion(majorVersion);
+            return latestVersion;
+        } catch (error: any) {
+            console.error(
+                chalk.red(`Failed to get the latest version for ${version}:`),
+                error.message,
+            );
+            return version.replace(".x", "");
+        }
     }
-  }
 
-  if (!semver.valid(version)) {
-    try {
-      const latestVersion = await getLatestVersion(version);
-      return latestVersion;
-    } catch (error: any) {
-      console.error(chalk.red(`Failed to get the latest version for invalid version format ${version}:`), error.message);
-      return "latest";
+    if (/^[><=^~]/.test(version)) {
+        try {
+            const [packageName, range] = version.split(/(?<=^\S+)\s/);
+            const latestVersion = await getLatestVersionForRange(
+                packageName,
+                range,
+            );
+            return latestVersion;
+        } catch (error: any) {
+            console.error(
+                chalk.red(
+                    `Failed to get the latest version for range ${version}:`,
+                ),
+                error.message,
+            );
+            return "latest";
+        }
     }
-  }
 
-  return version;
+    if (peerDependencies) {
+        const peerDepVersion = peerDependencies[version];
+        if (peerDepVersion) {
+            return peerDepVersion;
+        }
+    }
+
+    if (!semver.valid(version)) {
+        try {
+            const latestVersion = await getLatestVersion(version);
+            return latestVersion;
+        } catch (error: any) {
+            console.error(
+                chalk.red(
+                    `Failed to get the latest version for invalid version format ${version}:`,
+                ),
+                error.message,
+            );
+            return "latest";
+        }
+    }
+
+    return version;
 };
